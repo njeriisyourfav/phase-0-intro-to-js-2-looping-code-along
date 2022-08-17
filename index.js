c3a9f0d33757e4bd7384df36ee65655d725157df
lilian njeri ndungu <lilian.ndungu@student.moringaschool.com>
2022-08-17 14:57:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let newArr = [];
+for(let i=0; i<arr.length; i++) {
+    let message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+    newArr.push(message);
+}
+return newArr
+}
+
+function countDown(num) {
+while (num>=0) {
+    console.log(num)
+     num--;
+}   
+}
+console.log( countDown(10))
